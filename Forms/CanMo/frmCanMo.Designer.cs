7568cfc02b0cf4f6b7237f3b6aced102ba9e6409
abc <dungcntt97@gmail.com>
2021-10-19 16:05:55 +0700

    cmt

---
+++ Forms/CanMo/frmCanMo.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace BMS
+{
+	partial class frmCanMo
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnOK = new System.Windows.Forms.Button();
+			this.txtSoLuong = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnOK
+			// 
+			this.btnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnOK.Location = new System.Drawing.Point(131, 141);
+			this.btnOK.Name = "btnOK";
+			this.btnOK.Size = new System.Drawing.Size(120, 50);
+			this.btnOK.TabIndex = 1;
+			this.btnOK.Text = "OK";
+			this.btnOK.UseVisualStyleBackColor = true;
+			this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+			// 
+			// txtSoLuong
+			// 
+			this.txtSoLuong.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtSoLuong.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtSoLuong.Location = new System.Drawing.Point(42, 72);
+			this.txtSoLuong.Name = "txtSoLuong";
+			this.txtSoLuong.Size = new System.Drawing.Size(310, 49);
+			this.txtSoLuong.TabIndex = 0;
+			this.txtSoLuong.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSoLuong_KeyDown);
+			// 
+			// label1
+			// 
+			this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold);
+			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(398, 54);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "SỐ LƯỢNG MỠ ";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// frmCanMo
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(398, 194);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtSoLuong);
+			this.Controls.Add(this.btnOK);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "frmCanMo";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "SỐ LƯỢNG MỠ";
+			this.TopMost = true;
+			this.Load += new System.EventHandler(this.frmCanMo_Load);
+			this.Shown += new System.EventHandler(this.frmCanMo_Shown);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button btnOK;
+		private System.Windows.Forms.TextBox txtSoLuong;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ Forms/CanMo/frmCanMo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static BMS.frmProductCheckHistory1;
+
+namespace BMS
+{
+	public partial class frmCanMo : Form
+	{
+		public int row = 0;
+		public string column = "";
+		public double SL = 0;
+		internal SLCanMo _SLCanMo;
+
+		public frmCanMo()
+		{
+			InitializeComponent();
+		}
+
+		private void frmCanMo_Load(object sender, EventArgs e)
+		{
+			txtSoLuong.Focus();
+		}
+
+		private void btnOK_Click(object sender, EventArgs e)
+		{
+			SL += TextUtils.ToDouble(txtSoLuong.Text.Trim());
+			_SLCanMo(SL, row, column);
+			this.Close();
+			//this.DialogResult = DialogResult.OK;
+		}
+
+		private void txtSoLuong_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+				btnOK.Focus();
+		}
+
+		private void frmCanMo_Shown(object sender, EventArgs e)
+		{
+			txtSoLuong.Focus();
+		}
+	}
+}
